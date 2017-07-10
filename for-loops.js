716e2ea0934c4c3208d7eb05128806c0b6acd746
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-10 17:52:42 -0500

    Working my way through...

---
+++ for-loops.js
@@ -0,0 +1,17 @@
+/**
+ * Created by beelarr on 7/10/17.
+ */
+
+// for (let i = 5; i < 121; i +=10){
+//     console.log(i)
+// }
+
+// for (let i = 4096; i = 1; i /= 2){
+//     console.log(i)
+// }
+
+let presidents = ['George Washington', 'John Adams', 'Thomas Jefferson']
+
+for(let i in presidents){
+    console.log('President #' + (Number(i) + 1) + ' was ' + presidents[i])
+}
